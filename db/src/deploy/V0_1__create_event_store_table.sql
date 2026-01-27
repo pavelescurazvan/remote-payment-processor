535d583941afe8444e50224d1ffb22ad4e6c63ac
Pavelescu Razvan <pavelescu.razvan@gmail.com>
2026-01-27 21:32:24 +0200

    no message

--- db/src/deploy/V0_1__create_event_store_table.sql
+++ db/src/deploy/V0_1__create_event_store_table.sql
@@ -2,10 +2,14 @@ CREATE SCHEMA pay_pro;
 
 CREATE TABLE pay_pro.event_store (
   id BIGSERIAL PRIMARY KEY,
-  client INTEGER NOT NULL,
-  version BIGSERIAL NOT NULL UNIQUE,
   type VARCHAR NOT NULL,
-  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now()
-);
+  client INTEGER NOT NULL,
+  version BIGINT NOT NULL,
+  tx INTEGER NOT NULL,
+  amount BIGINT NOT NULL,
+  created_at TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(),
 
-CREATE INDEX event_store_client_idx ON pay_pro.event_store (client);
+  CONSTRAINT event_store_client_version_uk UNIQUE (client, version),
+  CONSTRAINT event_store_client_tx_type_uk UNIQUE (client, tx, type),
+  CONSTRAINT event_store_type_chk CHECK (type IN ('deposit','withdrawal','dispute','resolve','chargeback'))
+);
